9d972ebb1c50766bc2224ba2af0248266e8e8d03
Laurent <lg390@cam.ac.uk>
2014-06-27 07:01:01 +0200

    export apps

--- R/shinyApp.R
+++ R/shinyApp.R
@@ -3,7 +3,14 @@
 ##' @title A shiny app.
 ##' @return Used for its side effect
 ##' @author Laurent Gatto
+##' @aliases app1
 app2 <- function() {
     require("DESeq2")
     runApp(system.file("extdata/app2/", package = "rauthoring"))
+}
+
+
+app1 <- function() {
+    require("DESeq2")
+    runApp(system.file("extdata/app1/", package = "rauthoring"))
 }
